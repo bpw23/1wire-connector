02d999c0f2853999dd532c947bfa4e034b78dc7c
bpw23 <38862701+bpw23@users.noreply.github.com>
2019-11-08 22:56:40 +0100

    Update read1w.py
    Added
    * info field into 1wire.json with error informations, number of devices and last refresh
    * error handling on file writing

--- read1w.py
+++ read1w.py
@@ -1,51 +1,73 @@
 #!/usr/bin/python2 -u
 
-import ow
 import io
 import json
+import timeit
+import time
+
+import ow
 
 
 if __name__ == "__main__":
-    # read device list
-    with open('devices.json') as json_file:
-        devicelist = json.load(json_file)
+    while True:
+        # read device list
+        with open('devices.json') as json_file:
+            devicelist = json.load(json_file)
+        # outputlist as dict
+        outputlist = {}
 
-    # outputlist as dict
-    outputlist = {}
+        # connect to owfs
+        ow.init('localhost:4304')
 
-    # connect to owfs
-    ow.init('localhost:4304')
+	# Enable simultaneous temperature conversation
+	ow._put("simultaneous/temperature","1")
+	#ow._put("simultaneous/voltage","1")
 
-    # read in connected sensor
-    sensorlist = ow.Sensor('/').sensorList()
+        # read in connected sensor
+        sensorlist = ow.Sensor('/').sensorList()
+	#print(sensorlist)
 
-    # loop through sensors and read values
-    for sensor in sensorlist:
-        sensorvalues={}
-        #print('Family: ' + sensor.family)
-        #print('ID: ' + sensor.id)
-        #print('Type: ' + sensor.type)
-        try:
-	    #print('Name: ' + devicelist[sensor.id])
-	    sensorvalues["name"] = devicelist[sensor.id]
-	except KeyError:
-	    #print('Name: -')
-	    sensorvalues["name"] = " "
-        if sensor.family == "28":
-            sensorvalues["temperature10"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/temperature10")
-	    sensorvalues["power"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/power")
-        elif sensor.family == "26":
-	    sensorvalues["VAD"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/VAD")
-	    sensorvalues["VDD"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/VDD")
-	    sensorvalues["vis"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/vis")
-	    sensorvalues["humidity"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/humidity")
-        outputlist[sensor.family+"."+sensor.id]=sensorvalues
-        #print('----------------------------')
+        # loop through sensors and read values
+        for sensor in sensorlist:
+	    try:
+                sensorvalues={}
+                #print('Family: ' + sensor.family)
+                #print('ID: ' + sensor.id)
+                #print('Type: ' + sensor.type)
+                sensorvalues["name"] = devicelist[sensor.id]
 
-    # write to output json file
-    with io.open('1wire.json', 'w', encoding='utf8') as f:
-        f.write(unicode(json.dumps(outputlist, ensure_ascii=False, indent=4)))
+		if sensor.family == "28":
+                    sensorvalues["temperature10"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/temperature10")
+                    sensorvalues["power"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/power")
+                elif sensor.family == "26":
+                    sensorvalues["VAD"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/VAD")
+                    sensorvalues["VDD"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/VDD")
+                    sensorvalues["vis"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/vis")
+                    sensorvalues["humidity"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/humidity")
 
-    #print ('1wire.json written')
+            except KeyError:
+	        sensorvalues["name"] = ""
+		print("Error: The sensor [" + sensor.family + "." + sensor.id + "]  generates -sensorvalues KeyError-")
+		continue
 
+	    except ow.exUnknownSensor, e:
+    	        sensorvalues["Error"]="Sensor lost"
+		print("Sensor lost")
+		continue
 
+	    outputlist[sensor.family+"."+sensor.id] = sensorvalues
+	    #print('----------------------------')
+	# Put general infos into file
+	outputlist["info"] = {"error":"","Number_of_devices":len(sensorlist),"1wire_connector_version":"1","Last_refresh":time.strftime("%d.%m.%Y %H:%M:%S")}
+	
+	# Put info into file on error
+	if outputlist =="":
+	    outpulist={"info":{"error":"No devices found"}}
+
+	# write to output json file
+	try:
+	    with io.open('1wire.json', 'w', encoding='utf8') as f:
+	        f.write(unicode(json.dumps(outputlist, ensure_ascii=False, indent=4)))
+	    print ('1wire.json written - ' + time.strftime("%d.%m.%Y  %H:%M:%S"))
+	except IOError:
+	    print("Can't write 1wire.json !")
