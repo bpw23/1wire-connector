e2022fa62e6d4be6a96e94bfb2cabf5463a8633f
bpw23 <38862701+bpw23@users.noreply.github.com>
2019-11-08 23:48:15 +0100

    Update read1w.py
    Fixed errors, added BUS errors to info field

--- read1w.py
+++ read1w.py
@@ -44,10 +44,12 @@ if __name__ == "__main__":
                     sensorvalues["VDD"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/VDD")
                     sensorvalues["vis"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/vis")
                     sensorvalues["humidity"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/humidity")
+		    sensorvalues["pressure"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"B1-R1-A/pressure")
+		    sensorvalues["temperature"]=ow.owfs_get("/bus.0/"+sensor.family+"."+sensor.id+"/temperature")
 
             except KeyError:
 	        sensorvalues["name"] = ""
-		print("Error: The sensor [" + sensor.family + "." + sensor.id + "]  generates -sensorvalues KeyError-")
+		print("Error: The sensor [" + sensor.family + "." + sensor.id + "]  generates -sensorvalues KeyError- Set a name in devices.json!")
 		continue
 
 	    except ow.exUnknownSensor, e:
@@ -57,12 +59,17 @@ if __name__ == "__main__":
 
 	    outputlist[sensor.family+"."+sensor.id] = sensorvalues
 	    #print('----------------------------')
+
 	# Put general infos into file
-	outputlist["info"] = {"error":"","Number_of_devices":len(sensorlist),"1wire_connector_version":"1","Last_refresh":time.strftime("%d.%m.%Y %H:%M:%S")}
-	
-	# Put info into file on error
-	if outputlist =="":
-	    outpulist={"info":{"error":"No devices found"}}
+	info_error=""
+	if len(sensorlist) == 0:
+	    info_error="No 1wire-devices found!"
+	outputlist["info"] = {"error":info_error,"Number_of_devices":len(sensorlist),
+			      "1wire_connector_version":"1",
+			      "Last_refresh":time.strftime("%d.%m.%Y %H:%M:%S"),
+			      "CRC8_errors":ow.owfs_get("/statistics/errors/CRC8_errors"),
+			      "CRC16_errors":ow.owfs_get("/statistics/errors/CRC16_errors"),
+			      "BUS_detect_errors":ow.owfs_get("/statistics/errors/BUS_detect_errors")}
 
 	# write to output json file
 	try:
